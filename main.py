71164da8437fde4f3476eef80586cc9329038144
Isabel boaventura <boaventuraisabel31@gmail.com>
2021-08-27 00:02:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import os
 from flask import Flask, request, jsonify, send_from_directory
 
-DIRETORIO =""
+DIRETORIO ="D:\\projetos\\javascrypt_e_python\\Roger\\output"
 
 api = Flask(__name__)
 
 
 @api.route("/arquivos", methods=["GET"])
 def lista_arquivos():
-    pass
+    arquivos = []
+    for nome_do_arquivo in  os.listdir(DIRETORIO):
+        endereco_do_arquivo = os.path.join(DIRETORIO, nome_do_arquivo)
+        if(os.path.isfile(endereco_do_arquivo)):
+            arquivos.append(nome_do_arquivo)
+    
+    
+    return jsonify(arquivos)
+
 
 
 @api.route("/arquivos/<nome_do_arquivo>", methods=["GET"])
 def get_arquivo(nome_do_arquivo):
-    pass
+    return send_from_directory(DIRETORIO, nome_do_arquivo, as_attachment=True)
 
 
 @api.route("/arquivos", methods=["POST"])
 def post_arquivo():
-    pass
+    arquivo = request.files.get("meuArquivo")
+    
+    print(arquivo)
+    nome_do_arquivo = arquivo.filename        
+    arquivo.save(os.path.join(DIRETORIO, nome_do_arquivo))
+    
+    return '', 201
 
 if __name__=="__main__":
     api.run(debug=True, port=8000)
